6855766f58f707f61e5cac5b73e6e8e87c9ab97c
Nazim5 <61194035+Nazim5@users.noreply.github.com>
2020-03-10 12:57:58 +0000

    Some Changes
    in Feed.Java

--- src/ie/cct/objectorientedconstructs/s2018370s2018092/Feed.java
+++ src/ie/cct/objectorientedconstructs/s2018370s2018092/Feed.java
@@ -1,7 +1,40 @@
 package ie.cct.objectorientedconstructs.s2018370s2018092;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Collection;
+
 import ie.cct.objectorientedconstructs.FeedFactoryInterface;
+import ie.cct.objectorientedconstructs.FeedInterface;
+import ie.cct.objectorientedconstructs.FeedItem;
 
-public class Feed implements FeedFactoryInterface {
+public class Feed implements FeedInterface {
 
-}
+	@Override
+	public Collection<String> listTitles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public FeedItem getItem(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int numItems() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<FeedItem> findItems(String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	}
+
+
--- src/ie/cct/objectorientedconstructs/s2018370s2018092/FeedFactory.java
+++ src/ie/cct/objectorientedconstructs/s2018370s2018092/FeedFactory.java
@@ -1,7 +1,17 @@
 package ie.cct.objectorientedconstructs.s2018370s2018092;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+
 import ie.cct.objectorientedconstructs.FeedFactoryInterface;
+import ie.cct.objectorientedconstructs.FeedInterface;
 
 public class FeedFactory implements FeedFactoryInterface {
+
+	@Override
+	public FeedInterface createFeed(BufferedReader in) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
